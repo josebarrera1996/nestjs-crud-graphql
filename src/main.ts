10a185bb4bcf525e6cea1dcd1ae40068c84fe3ed
José Barrera <josebarrerag1996@gmail.com>
2023-05-12 10:21:56 -0300

    Implementando Validaciones

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Pipe para segurar que todos los endpoints de la app esten proteguidos de datos incorrectos
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/posts/dto/create-post.input.ts
+++ src/posts/dto/create-post.input.ts
@@ -1,15 +1,20 @@
 /* eslint-disable prettier/prettier */
-
 import { Field, InputType } from "@nestjs/graphql";
+import { IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
 // DTO de 'Posts'
 // Servirá para detallar que es lo que recibirá del cliente (los datos)
+// Y para implementar validaciones
 @InputType()
 export class CreatePostInput {
 
+    @IsNotEmpty({ message: "Title can't be empty" })
+    @MinLength(10, { message: 'Title is too short' })
+    @MaxLength(100, { message: 'Title is too long' })
     @Field()
     title: string;
 
-    @Field()
-    content: string;
+    @MaxLength(400, { message: 'Title is too long' })
+    @Field({ nullable: true })
+    content?: string;
 }
